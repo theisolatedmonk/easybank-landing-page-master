b605c10419473b42b5648040ad444a94667f66df
theisolatedmonk <96rahulseth@gmail.com>
2023-08-07 21:04:20 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -7,39 +7,28 @@ import mobile_grouped from "@/images/image-mockups.png";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen bg-white gap-4 justify-between">
+    <main className="w-full h-screen bg-white gap-4 justify-between text-black overflow-auto">
       <div className="flex justify-between px-2 py-4  bg-white z-20 relative">
         <button>
           {" "}
           <Image src={logo} alt={""} />
+          <div className="flex gap-4">
+            <p className="hover:border-b-2 border-b-green-500">home</p>
+            <p className="hover:border-b-2 border-b-green-500">About</p>
+            <p className="hover:border-b-2 border-b-green-500">Contact</p>
+            <p className="hover:border-b-2 border-b-green-500">Blog</p>
+            <p className="hover:border-b-2 border-b-green-500">Careers</p>
+          </div>
         </button>
-        <button className="flex flex-col gap-1  ">
+        <button className="flex flex-col gap-1  sm:hidden">
           <p className="w-6 bg-black h-[.5px] "></p>
           <p className="w-6  bg-black h-[.5px] "></p>
           <p className="w-6  bg-black h-[1px] "></p>
         </button>
       </div>
-      <div className="flex sm:flex-col-reverse">
-        <div className="">
-          <div className="  ">
-            <Image
-              className=" z-0 sm:hidden fixed top-[-4px]"
-              src={bg_intro_mobile}
-              alt={""}
-            />
-            <Image
-              className="fixed   z-10 top-[-75px] w-[95%] left-2"
-              src={mobile_grouped}
-              alt={""}
-            />
-          </div>
-          <Image
-            className="hidden sm:flex z-0 "
-            src={bg_intro_desktop}
-            alt={""}
-          />
-        </div>
-        <div className="flex flex-col">
+      <div className="flex    sm:flex-row flex-col-reverse w-full  justify-between border-2 ">
+        {/* text */}
+        <div className="flex flex-col top-96 sm:top-20">
           <p className="">Next generation digital banking</p>
           <p className="">
             Take your financial life online. Your Easybank account will be a
@@ -48,7 +37,39 @@ export default function Home() {
           </p>
           <button> Request Invite</button>
         </div>
+        {/* image */}
+        <div
+          className="  border-2  sm:w-1/2 border-red-400 
+        "
+        >
+          <div className=" sm:hidden relative h-[200px]   ">
+            <Image
+              className=" z-0    absolute"
+              src={bg_intro_mobile}
+              alt={""}
+            />
+            <Image
+              className="  absolute  z-10  "
+              src={mobile_grouped}
+              alt={""}
+            />
+          </div>
+
+          <div className=" hidden sm:flex min-w-1/2   relative ">
+            <Image
+              className="hidden sm:flex z-0   "
+              src={bg_intro_desktop}
+              alt={""}
+            />
+            <Image
+              className="absolute   z-10  "
+              src={mobile_grouped}
+              alt={""}
+            />
+          </div>
+        </div>
       </div>
     </main>
   );
 }
+1;
