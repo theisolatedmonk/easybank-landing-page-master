be60bc051605304f6854cae8a3045a3ee30df6c8
theisolatedmonk <96rahulseth@gmail.com>
2023-08-08 20:24:53 +0530

    hero section imlimented

--- app/page.tsx
+++ app/page.tsx
@@ -12,13 +12,19 @@ export default function Home() {
         <button>
           {" "}
           <Image src={logo} alt={""} />
-          <div className="flex gap-4">
-            <p className="hover:border-b-2 border-b-green-500">home</p>
-            <p className="hover:border-b-2 border-b-green-500">About</p>
-            <p className="hover:border-b-2 border-b-green-500">Contact</p>
-            <p className="hover:border-b-2 border-b-green-500">Blog</p>
-            <p className="hover:border-b-2 border-b-green-500">Careers</p>
-          </div>
+        </button>
+        <div className="sm:flex gap-4 hidden">
+          <p className="hover:border-b-2 border-b-green-500">home</p>
+          <p className="hover:border-b-2 border-b-green-500">About</p>
+          <p className="hover:border-b-2 border-b-green-500">Contact</p>
+          <p className="hover:border-b-2 border-b-green-500">Blog</p>
+          <p className="hover:border-b-2 border-b-green-500">Careers</p>
+        </div>
+        <button
+          type="button"
+          className="sm:flex hidden bg-gradient-to-r from-[hsl(136,65%,51%)] to-[hsl(192,70%,51%)] hover:from-pink-500 hover:to-yellow-500 py-2 px-4 rounded-3xl"
+        >
+          Request Invite
         </button>
         <button className="flex flex-col gap-1  sm:hidden">
           <p className="w-6 bg-black h-[.5px] "></p>
@@ -26,47 +32,62 @@ export default function Home() {
           <p className="w-6  bg-black h-[1px] "></p>
         </button>
       </div>
-      <div className="flex    sm:flex-row flex-col-reverse w-full  justify-between border-2 ">
+      <div className="flex    sm:flex-row flex-col-reverse w-full  justify-between  gap-32 sm:gap-4">
         {/* text */}
-        <div className="flex flex-col top-96 sm:top-20">
+        <div className="flex flex-col top-96 sm:top-20 items-center justify-center p-4 gap-4 sm:p-14">
           <p className="">Next generation digital banking</p>
           <p className="">
             Take your financial life online. Your Easybank account will be a
             one-stop-shop for spending, saving, budgeting, investing, and much
             more.{" "}
           </p>
-          <button> Request Invite</button>
+          <button
+            type="button"
+            className="bg-gradient-to-r from-[hsl(136,65%,51%)] to-[hsl(192,70%,51%)] hover:from-pink-500 hover:to-yellow-500 py-2 px-4 rounded-3xl"
+          >
+            Request Invite
+          </button>
         </div>
         {/* image */}
         <div
-          className="  border-2  sm:w-1/2 border-red-400 
+          className="     sm:w-[120%] 
         "
         >
           <div className=" sm:hidden relative h-[200px]   ">
             <Image
-              className=" z-0    absolute"
+              className=" z-0    absolute "
               src={bg_intro_mobile}
               alt={""}
             />
             <Image
-              className="  absolute  z-10  "
+              className="  absolute  z-10 mt-[-125px] w-[94%] ml-2"
               src={mobile_grouped}
               alt={""}
             />
           </div>
 
-          <div className=" hidden sm:flex min-w-1/2   relative ">
+          <div className=" hidden sm:flex  relative w-[150%] mt-[-150px] ml-[-140px]">
             <Image
-              className="hidden sm:flex z-0   "
+              className="hidden sm:flex z-0  ml-20   "
               src={bg_intro_desktop}
               alt={""}
             />
             <Image
-              className="absolute   z-10  "
+              className="absolute   z-10  ml-[230px] w-[67%] mt-16"
               src={mobile_grouped}
               alt={""}
             />
           </div>
+        </div>
+      </div>
+
+      <div className="bg-[hsl(233,8%,62%)] bg-opacity-30">
+        <div className="flex flex-col sm:p-14 w-[68%]">
+          <p className="">Who choose Easybank?</p>
+          <p className="">
+            We leverage Open Banking to turn your bank account into your
+            financial hub Control your finances like never before.
+          </p>
         </div>
       </div>
     </main>
